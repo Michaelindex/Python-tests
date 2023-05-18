8d707681c9dfbd206dc75aba40611db4db191569
Michael Santos <mcialbr123@gmail.com>
2023-05-17 21:58:06 -0300

    tentando..

---
+++ ppt/pedrapapeltesoura.py
@@ -0,0 +1,49 @@
+import random
+numeroMaquina = random.randint(0,2)
+
+if numeroMaquina == 0:
+    numeroMaquina = "Pedra"
+elif numeroMaquina == 1:
+    numeroMaquina = "Papel"
+else:
+    numeroMaquina = "Tesoura"
+
+print("---- VAMOS JOGAR PEDRA PAPEL OU TESOURA ----")
+print(f'1 = Pedra \n2 = Papel \n3 = Tesoura')
+
+while True:
+    usr = int(input("Diga um numero de 1 a 3"))
+    if usr == 1:
+        usr = "Pedra"
+        break
+    elif usr == 2:
+        usr = "Papel"
+        break
+    elif usr == 3:
+        usr = "Tesoura"
+        break
+    else:
+        print("Numeros de um a três pls...")
+
+while usr != "win":
+    if numeroMaquina == 0:
+        numeroMaquina = "Pedra"
+    elif numeroMaquina == 1:
+        numeroMaquina = "Papel"
+    else:
+        numeroMaquina = "Tesoura"
+    print("O numero da maquina ja foi escolhida")
+    
+    print("Você só saíra daqui se ganhar da maquina")
+    if numeroMaquina == "Pedra" and usr == "Tesoura":
+        print("A maquina venceu por escolher", numeroMaquina)
+    elif numeroMaquina == "Papel" and usr == "Pedra":
+        print("A maquina venceu por escolher", numeroMaquina)
+    elif numeroMaquina == "Tesoura" and usr == "Papel":
+        print("A maquina venceu por escolher", numeroMaquina)
+    else:
+        print("Você venceu por escolher: ", usr, "Contra: ", numeroMaquina,"Da Maquina !!!")
+        usr = "win"
+
+
+
