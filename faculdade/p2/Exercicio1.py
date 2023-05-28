2e2745bb3fb8c91e53202fe44601c7528cb6bb98
Michael Santos <mcialbr123@gmail.com>
2023-05-27 22:59:05 -0300

    Create Exercicio1.py

---
+++ faculdade/p2/Exercicio1.py
@@ -0,0 +1,21 @@
+
+print("Loja do Johnata suzarte souza (RU:3996609)") # RU: 4039661
+
+valor_unitario = float(input("Digite o valor unitário do produto: "))
+quantidade = int(input("Digite a quantidade do produto: "))
+
+totalsemdesconto = valor_unitario * quantidade
+
+if quantidade <= 9:
+    desc = 0
+elif quantidade <= 99:
+    desc = 0.05
+elif quantidade <= 999:
+    desc = 0.1
+else:
+    desc = 0.15
+
+comdesconto = totalsemdesconto - (totalsemdesconto * desc)
+
+print("Valor total sem desconto: R$", totalsemdesconto)
+print("Valor total com desconto: R$", comdesconto)
