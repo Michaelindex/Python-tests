d0bdc28d239f866519904edb4fd40697dd99d8cb
Michael Santos <mcialbr123@gmail.com>
2023-06-10 22:33:15 -0300

    media

--- media/calcmedia.py
+++ media/calcmedia.py
@@ -3,4 +3,8 @@ um = int(input("Digite o primeiro numero: "))
 dois = int(input("Digite o segundo numero: "))
 tres = int(input("Digite o terceiro numero: "))
 quatro = int(input("Digite o Quarto numero: "))
+#Processo
 
+calc = (um + dois + tres + quatro)
+
+print(calc)
