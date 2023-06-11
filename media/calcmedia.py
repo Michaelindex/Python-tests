d02af10bc832330fbfd5aa71ee639442083bd089
Michael Santos <mcialbr123@gmail.com>
2023-06-10 22:31:56 -0300

    Update calcmedia.py

--- media/calcmedia.py
+++ media/calcmedia.py
@@ -1,4 +1,4 @@
-um = int(input("Digite o primeiro numero"))
-dois = int(input("Digite o segundo numero"))
-tres = int(input("Digite o terceiro numero"))
-quatro = int(input("Digite o Quarto numero"))
+um = int(input("Digite o primeiro numero: "))
+dois = int(input("Digite o segundo numero: "))
+tres = int(input("Digite o terceiro numero: "))
+quatro = int(input("Digite o Quarto numero: "))
