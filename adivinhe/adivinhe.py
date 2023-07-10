d19857aad2a3ecee6c027d9a5726c3a4e00638fd
Michael Santos <mcialbr123@gmail.com>
2023-07-09 22:54:19 -0300

    Update adivinhe.py

--- adivinhe/adivinhe.py
+++ adivinhe/adivinhe.py
@@ -5,10 +5,10 @@ comp = random.randint(0,101)
 
 print(comp)
 while usr != comp:
-    usr = int(input("Numero errado, digite o numero novamente"))
+    usr = int(input("Numero errado, digite o numero novamente: "))
     if usr > comp:
         print("Um pouco menos")
-    elif comp < usr:
+    else:
         print("Um pouco mais")
 
 print("BUENA MANITO")
