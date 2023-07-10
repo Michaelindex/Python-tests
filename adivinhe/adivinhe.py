73f05f30580393a3c518b437c15c906a60736273
Michael Santos <mcialbr123@gmail.com>
2023-07-09 22:48:13 -0300

    Update adivinhe.py

--- adivinhe/adivinhe.py
+++ adivinhe/adivinhe.py
@@ -1,8 +1,8 @@
 import random
-
+print("TENTE ADIVINHAR")
 usr = int(input("Digite um numero de 1 a 100: "))
 comp = random.randint(0,101)
 
 print(comp)
-if usr != comp:
-    print('MAOI')
+while usr != comp:
+    usr = int(input("Numero errado, digite o numero novamente"))
