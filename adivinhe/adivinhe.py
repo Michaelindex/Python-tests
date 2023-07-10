d7f14bc298c413d8a1ae03873cffda84f9b91f26
Michael Santos <mcialbr123@gmail.com>
2023-07-09 23:02:45 -0300

    Update adivinhe.py

--- adivinhe/adivinhe.py
+++ adivinhe/adivinhe.py
@@ -1,19 +1,43 @@
 import random
+import tkinter as tk
 
 def adivinhar_numero():
     comp = random.randint(1, 100)
     tentativas = 0
 
-    while True:
-        usr = int(input("Digite um número de 1 a 100: "))
+    def verificar_numero():
+        nonlocal tentativas
+        usr = int(entry.get())
         tentativas += 1
 
         if usr > comp:
-            print("Um pouco menos")
+            resultado_label.config(text="Um pouco menos")
         elif usr < comp:
-            print("Um pouco mais")
+            resultado_label.config(text="Um pouco mais")
         else:
-            print("BOAA! Você acertou em", tentativas, "tentativa(s)!")
-            break
+            resultado_label.config(text=f"BOAA! Você acertou em {tentativas} tentativa(s)!")
+
+    # Configuração da janela principal
+    janela = tk.Tk()
+    janela.title("Jogo de Adivinhação")
+    janela.geometry("300x200")
+
+    # Rótulo e campo de entrada
+    instrucao_label = tk.Label(janela, text="Digite um número de 1 a 100:")
+    instrucao_label.pack()
+
+    entry = tk.Entry(janela)
+    entry.pack()
+
+    # Botão de envio
+    enviar_button = tk.Button(janela, text="Enviar", command=verificar_numero)
+    enviar_button.pack()
+
+    # Rótulo de resultado
+    resultado_label = tk.Label(janela, text="")
+    resultado_label.pack()
+
+    # Inicia a interface gráfica
+    janela.mainloop()
 
 adivinhar_numero()
