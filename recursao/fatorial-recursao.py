4475ae52cc263795a02d88006ccff0a53b12fb38
Michael Santos <mcialbr123@gmail.com>
2023-09-15 11:34:43 -0300

    tentando

--- recursao/fatorial-recursao.py
+++ recursao/fatorial-recursao.py
@@ -1,10 +1,4 @@
-def fatorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * fatorial(n - 1)
-numero = int(input('Informe um número inteiro: '))
-if numero >= 0:
-    print(fatorial(numero))
-else:
-    print('Números negativos não são permitidos!')
+fatorial = int(input("Informe um número inteiro: "))
+i = 1
+for i in range(fatorial):
+    print(i * fatorial)
