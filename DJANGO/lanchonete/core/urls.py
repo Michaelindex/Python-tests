6d5780dfc81d984c3d6fbec351e355304833cbb1
Michael Santos <mcialbr123@gmail.com>
2023-09-21 10:48:53 -0300

    att

--- DJANGO/lanchonete/core/urls.py
+++ DJANGO/lanchonete/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('lanche/<int:lanche_id>', lanche, name='lanche'),
     path('buscar', buscar, name='buscar')
 ]
---
+++ DJANGO/lanchonete/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO lanche_lanche(
+    nome, preco, categoria, ingredientes, publicado, date_lanche, funcionario_id)
+    ('X-CHURRASCI', 38, 'Churrasco', 'Pão, picanha acebolada, Mussarela Alface', 1, '2023-09-21', 1),
+    ('X-BACON', )
