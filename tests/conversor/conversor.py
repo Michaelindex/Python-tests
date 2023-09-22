d012cfd2b1119704b892bdfb99e15583075798fb
Michael Santos <mcialbr123@gmail.com>
2023-09-21 23:43:47 -0300

    Update conversor.py

--- tests/conversor/conversor.py
+++ tests/conversor/conversor.py
@@ -2,5 +2,4 @@ def conversor():
     print("Aqui faremos a conversão da unidade que você escolher")
     entrada1 = int(input("Qual unidade você vai começar\n1 - mm\n2 - cm\n3 - m\n4 - km\nEscolha: "))
     entrada2 = int(input("Para qual unidade você vai converter\n1 - mm\n2 - cm\n3 - m\n4 - km\nEscolha: "))
-    
-    
+    entrada3 = float(input("Aqui digite o valor que você deseja converter: "))
