ed999bf856276243c55e4d4423e9ed195b1149d2
Michael Santos <mcialbr123@gmail.com>
2023-09-22 01:06:48 -0300

    att

--- tests/lista de tarefas/tarefas.py
+++ tests/lista de tarefas/tarefas.py
@@ -7,20 +7,44 @@ conn = sqlite3.connect('TAREFAS.db')#CHATGPT
 cursor = conn.cursor()#CHATGPT
 cursor.execute('CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY, titulo TEXT, corpo TEXT)')#CHATGPT
 
-#Inserir dados #CHATGPT
-cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',('Estudos', 'Preciso estudar Python')) #CHATGPT
-cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',('Estudos', 'Preciso estudar JavaScript')) #CHATGPT
-cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',('Trabalho', 'Preciso estudar Pfsense')) #CHATGPT
-cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',('Trabalho', 'Preciso criar o desenho da Infra')) #CHATGPT
-
 #Consultar dados #CHATGPT
-cursor.execute('SELECT * FROM tarefas') #CHATGPT
-for row in cursor.fetchall(): #CHATGPT
-    print(row) #CHATGPT
+def consulta():
+    cursor.execute('SELECT * FROM tarefas') #CHATGPT
+    for row in cursor.fetchall(): #CHATGPT
+        print(row) #CHATGPT
+
+#Inserir dados #CHATGPT
+def adicionar():
+    titulo = input("Digite o título da tarefa.")
+    corpo = input("Digite o corpo da tarefa")
+    cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',(titulo, corpo)) #CHATGPT
+
+#Remover dados
+def remover():
+    consulta()
+    removid = print("Digite o ID da tarefa que você deseja remover")
+    cursor.execute('DELETE FROM tarefas WHERE id = ?', (removid))
+    app()
+
+def app():
+    print("App de Tarefas - 2023v1")
+    desejo = int(input("O que você quer fazer no seu App de Tarefas?\n1 - Consultar\n2 - Adicionar\n3 - Remover\n4 - Sair"))
+    if desejo == 1:
+        consulta()
+        app()
+    elif desejo == 2:
+        adicionar()
+        app()
+    elif desejo == 3:
+        remover()
+        app()
+    elif desejo == 4:
+        print("Tchau !")
+    else:
+        print("Escolha ")
+
+        
 
 conn.close() #CHATGPT
 
 
-# print("-----LISTANDO SUAS TAREFAS-----")
-# def tarefas():
-#     escolha = int(input("Digite o que você deseja fazer\n1 - Consultar tarefas\n2 - Adicionar tarefas\n3 - Remover tarefas"))
