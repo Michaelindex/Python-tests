76e603ba800465e7b8ae5768b84992a6fb2e57c1
Michael Santos <mcialbr123@gmail.com>
2023-09-21 23:01:04 -0300

    Update calcsimples.py

--- tests/Calculadora Simples/calcsimples.py
+++ tests/Calculadora Simples/calcsimples.py
@@ -1,9 +1,9 @@
 print("Calculadora Simples")
-num1 = input(float("Entre com o primeiro numero"))
-num2 = input(float("Entre com o segundo numero"))
+num1 = float(input("Entre com o primeiro numero: "))
+num2 = float(input("Entre com o segundo numero: "))
 
 print("Escolha agora o que você quer fazer com esses 2 números informados")
-user = input(int(f"1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão"))
+user = int(input(f"1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nEscolha: "))
 
 def calculadora():
     if user == 1:
