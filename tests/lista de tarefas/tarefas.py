49f63589ead9635de16b4938d1cd25951e11b814
Michael Santos <mcialbr123@gmail.com>
2023-09-22 01:16:20 -0300

    Update tarefas.py

--- tests/lista de tarefas/tarefas.py
+++ tests/lista de tarefas/tarefas.py
@@ -1,4 +1,5 @@
 import sqlite3#CHATGPT
+import keyboard#CHATGPT
 
 #Conectar ao banco de dados ( ou criar um novo )#CHATGPT
 conn = sqlite3.connect('TAREFAS.db')#CHATGPT
@@ -12,18 +13,23 @@ def consulta():
     cursor.execute('SELECT * FROM tarefas') #CHATGPT
     for row in cursor.fetchall(): #CHATGPT
         print(row) #CHATGPT
+    app()
 
 #Inserir dados #CHATGPT
 def adicionar():
     titulo = input("Digite o título da tarefa.")
     corpo = input("Digite o corpo da tarefa")
     cursor.execute('INSERT INTO tarefas (titulo, corpo) VALUES (?, ?)',(titulo, corpo)) #CHATGPT
+    sair = input(f"Se deseja continuar aperte 'ENTER'\nSe deseja parar digite 'ESC'\nFaça: ")
+    event = keyboard.read_event()#CHATGPT
+
 
 #Remover dados
 def remover():
     consulta()
     removid = print("Digite o ID da tarefa que você deseja remover")
     cursor.execute('DELETE FROM tarefas WHERE id = ?', (removid))
+    print("REMOVIDO !!!")
     app()
 
 def app():
@@ -31,20 +37,17 @@ def app():
     desejo = int(input("O que você quer fazer no seu App de Tarefas?\n1 - Consultar\n2 - Adicionar\n3 - Remover\n4 - Sair"))
     if desejo == 1:
         consulta()
-        app()
     elif desejo == 2:
         adicionar()
-        app()
     elif desejo == 3:
         remover()
-        app()
     elif desejo == 4:
         print("Tchau !")
     else:
         print("Escolha um numero entre 1 a 4")
         app()
 
-        
+app()
 
 conn.close() #CHATGPT
 
