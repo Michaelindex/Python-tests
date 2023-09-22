eb6570a92404ed215e5f81f3ea8c7f7a1e080925
Michael Santos <mcialbr123@gmail.com>
2023-09-21 23:20:07 -0300

    Update calcsimples.py

--- tests/Calculadora Simples/calcsimples.py
+++ tests/Calculadora Simples/calcsimples.py
@@ -1,18 +1,8 @@
-print("Calculadora Simples")
-num1 = float(input("Entre com o primeiro numero: "))
-num2 = float(input("Entre com o segundo numero: "))
-
-print("Escolha agora o que você quer fazer com esses 2 números informados")
-user = int(input(f"1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nEscolha: "))
-
-def refaz():
-    fim = input(f"Deseja finalizar? Digite 'sim' ou 's'\nSe não só digite outra coisa: ".upper())
-    if fim =='SIM' or 'S':
-        calculadora()
-    else:
-        print("obrigado.")
-        
 def calculadora():
+    num1 = float(input("Entre com o primeiro numero: "))
+    num2 = float(input("Entre com o segundo numero: "))
+    print("Escolha agora o que você quer fazer com esses 2 números informados")
+    user = int(input(f"1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\nEscolha: "))
     if user == 1:
         result = num1 + num2
         print(result)
@@ -32,4 +22,13 @@ def calculadora():
     else:
         print("Por favor, digite um numero entre 1 a 4.")
         calculadora()
+
+def refaz():
+    fim = input(f"Deseja recomeçar? Digite 'sim' ou 's'\nSe não só digite outra coisa: ")
+    if fim == 'SIM' or 'S' or 'sim' or 's':
+        calculadora()
+    elif fim != 'SIM' or 'S':
+        print('Thank You!!!')
+
+
 calculadora()
