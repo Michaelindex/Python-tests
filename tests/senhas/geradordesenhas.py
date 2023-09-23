c8689dd65476ceab957bdb980e4903b4c58576b8
Michael Santos <mcialbr123@gmail.com>
2023-09-23 01:28:35 -0300

    att

--- tests/senhas/geradordesenhas.py
+++ tests/senhas/geradordesenhas.py
@@ -10,16 +10,17 @@ janela = tk.Tk()
 janela.geometry("300x200")
 janela.eval('tk::PlaceWindow . center')
 janela.title("GERADOR DE SENHAS")
-icon = tk.PhotoImage(file = 'tests/senhas/github.png ')
+icon = tk.PhotoImage(file = 'tests/senhas/update.png ')
 janela.iconphoto(True, icon)
 
 # Criar um rótulo
 label = tk.Label(janela, text="---BEM VINDO AO GERADOR DE SENHAS 2000v1---")
 label.pack(padx=20,pady=50)#AQUI ESTA DANDO ERRO
+label = tk.Label(janela, text="Clique no botão abaixo para gerar a senha")
 label.pack()
 
 # Criar um botão
-botao = tk.Button(janela, text="Clique Aqui", command=clique_botao)
+botao = tk.Button(janela, text="GERAR", command=clique_botao)
 botao.pack()
 
 # Iniciar o loop da interface gráfica
