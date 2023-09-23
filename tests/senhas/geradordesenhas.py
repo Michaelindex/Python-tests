e5234ce7a8705c224b3dc3c9c83f51620bd55780
Michael Santos <mcialbr123@gmail.com>
2023-09-23 01:54:45 -0300

    att

--- tests/senhas/geradordesenhas.py
+++ tests/senhas/geradordesenhas.py
@@ -1,8 +1,16 @@
+import pyperclip
+import random
 import tkinter as tk
 from tkinter import *
 
 def clique_botao():
     label.config(text="Botão Clicado!")
+
+def randomizar():
+    senharamdom = random.randint('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#')
+    for i in senharamdom (0,12,1):
+        senha = senha + senharamdom
+    # pyperclip.copy("")
 
 # Criar a janela principal
 janela = tk.Tk()
@@ -18,7 +26,7 @@ label.pack(padx=20,pady=30)
 label.pack()
 
 # Criar um botão
-botao = tk.Button(janela, text="CLICK PARA GERAR E COPIAR", command=clique_botao)
+botao = tk.Button(janela, text="CLICK PARA GERAR E COPIAR", command=randomizar)
 botao.pack()
 
 # Iniciar o loop da interface gráfica
