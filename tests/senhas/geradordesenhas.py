8d9f432ccf4a890b0ec35cad200422e8e756bccf
Michael Santos <mcialbr123@gmail.com>
2023-09-23 01:24:27 -0300

    att

--- tests/senhas/geradordesenhas.py
+++ tests/senhas/geradordesenhas.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 import tkinter
 
 def clique_botao():
@@ -8,11 +9,13 @@ def clique_botao():
 janela = tk.Tk()
 janela.geometry("300x200")
 janela.eval('tk::PlaceWindow . center')
-janela.title("Exemplo Tkinter")
+janela.title("GERADOR DE SENHAS")
+icon = tk.PhotoImage(file = 'tests/senhas/github.png ')
+janela.iconphoto(True, icon)
 
 # Criar um rótulo
 label = tk.Label(janela, text="---BEM VINDO AO GERADOR DE SENHAS 2000v1---")
-label.pack(padx=100,pady=5)#AQUI ESTA DANDO ERRO
+label.pack(padx=20,pady=50)#AQUI ESTA DANDO ERRO
 label.pack()
 
 # Criar um botão
