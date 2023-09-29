c3dc408a85c1a4a59e2af60b79731a24b107f7b2
Michael Santos <mcialbr123@gmail.com>
2023-09-29 10:15:32 -0300

    att

--- DJANGO/lanchonete/lanchonete/urls.py
+++ DJANGO/lanchonete/lanchonete/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings #Para funcionar imagens
 from django.conf.urls.static import static #Para funcionar imagens
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls'))   
+    path('', include('core.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Para funcionar imagens
--- DJANGO/lanchonete/usuarios/views.py
+++ DJANGO/lanchonete/usuarios/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login(request):
-    pass
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            messages.error(request, "Os Campos email e senha não podem ficar")
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list( 'username', flat=True).get()
+            user = auth.authenticate(request, username=nome, poassword=senha)
+            print(nome, user)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'Login realizado com sucesso')
+                return redirect('dashboard')
+            else:
+                messages.error(request,'A senha está incorreta!')
+        else:
+            messages.error(request, 'Digite um e-mail válido!')
+    return render(request, 'login.html')
 
 def cadastro(request):
     pass
